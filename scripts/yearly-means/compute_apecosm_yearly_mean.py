4e5fad46d4b0a1a76e16680e755243df899e40ad
barriern <nicolas.barrier@ird.fr>
2020-12-16 10:46:29 +0100

    Adding comments

--- scripts/yearly-means/compute_apecosm_yearly_mean.py
+++ scripts/yearly-means/compute_apecosm_yearly_mean.py
@@ -1,32 +1,47 @@
+''' Computes the yearly averages of Apecosm outputs. 
+    Yearly averages are computed from May (month 05) to April (month 04).
+    
+    One file per year is saved, since Apecosm outputs are too heavy.
+'''
+
 import xarray as xr
 import os.path
 from datetime import datetime
 import numpy as np
 import subprocess
 
+
+''' Computes the yearly mean for Apecosm outputs. One file per year is saved. '''
 def compute_yearly_mean(year, month, value, varname):
 
+    # extracts the year to process, remove the last one 
+    # since incomplete mean
     ytt = np.unique(year)
-    date = year * 100 + month
-
     ytt = ytt[:-1]
+    
+    date = year * 100 + month
 
     nyears = len(ytt)
 
     dimnames = value.dims
 
+    # Loop over the years to process
     for y in ytt:
         print(y)
 
+        # extracts the time index to use in the averaging
         test = (year == y) & (month >=5)
         test = test | ((year == (y + 1)) & (month < 5))
         iok = np.nonzero(test)[0]
+        
+        # computes the time mean, stored as DataArray
+        # output is now of size (y, x, com, size)
         output = value.isel(time=iok).mean(dim='time').values
-        print(output.shape)
-    
+        
+        # create the output dataset
         dataout = xr.Dataset()
-        dataout[varname] = (dimnames, output[np.newaxis])
-        dataout[dimnames[0]] = ([dimnames[0]], [y])
+        dataout[varname] = (dimnames, output[np.newaxis])  # add shadow time dimension
+        dataout[dimnames[0]] = ([dimnames[0]], [y]) 
         dataout.attrs['file'] = os.path.realpath(__file__)
         dataout.attrs['date'] = str(datetime.today())
         dataout.attrs['description'] = 'mean over %s' %(str(date[iok]))
@@ -35,24 +50,30 @@ def compute_yearly_mean(year, month, value, varname):
 
     return dataout
 
+# list of apecosm simulation to process
 preflist = ['debugged_corr_mask']
 
-for prefix in preflist:
+# list of variables to process
+varlist = ['OOPE']
 
+# loop over all simulations
+for prefix in preflist:
+    
+    # extract input dir.
     dirin = '/home/datawork-marbec-pmod/outputs/APECOSM/ORCA1/%s/output' %prefix
 
+    # creates the output directory (input_dir/yearly)
     dirout = '%s/yearly' %dirin
     subprocess.run(["mkdir", "-p", dirout])
     print(dirout)
 
-    preflist = ['OOPE']
+    # loop over al variables
+    for var in varlist:
 
-    for pref in preflist:
-
-        print('++++++++++++++++++++++++++++++++++ Computing %s yearly mean' %pref)
-
-        data = xr.open_mfdataset('%s/*%s*nc' %(dirin, pref), combine='by_coords')
-        data = data[pref]
+        print('++++++++++++++++++++++++++++++++++ Computing %s yearly mean' %var)
+        # open all files in one raw and compute average
+        data = xr.open_mfdataset('%s/*%s*nc' %(dirin, var), combine='by_coords')
+        data = data[var]
         year = data['time.year'].values
         month = data['time.month'].values
-        output = compute_yearly_mean(year, month, data, pref)
+        output = compute_yearly_mean(year, month, data, var)
--- scripts/yearly-means/compute_pisces_yearly_mean.py
+++ scripts/yearly-means/compute_pisces_yearly_mean.py
@@ -1,31 +1,44 @@
+''' Computes the yearly averages of NEMO-Pisces outputs. 
+    Yearly averages are computed from May (month 05) to April (month 04)
+'''
+
 import xarray as xr
 import os.path
 from datetime import datetime
 import numpy as np
 
+''' Computes the yearly mean. Returns a Dataset '''
 def compute_yearly_mean(year, month, value, varname):
 
+    # extracts the years to process
     ytt = np.unique(year)
     ytt = ytt[:-1]   # remove the last year (since not full average)
     nyears = len(ytt)
     
+    # converts years/month into date
     date = year * 100 + month
 
+    # get the size of the output array
     outdims = value.isel(time_counter=slice(0, nyears)).shape
+    
+    # extracts the names of the array dimension
     dimnames = value.dims
 
+    # init the output array
     output = np.zeros(outdims)
 
     cpt = 0
     for y in ytt:
-
+        
+        # extracts the time index that corresponds to one year (from J to A)
         test = (year == y) & (month >=5)
         test = test | ((year == (y + 1)) & (month < 5))
         iok = np.nonzero(test)[0]
-        print(date[iok])
+        # computes the mean over the time dimension
         output[cpt] = value.isel(time_counter=iok).mean(dim='time_counter')
         cpt += 1
 
+    # converts numpy to dataset
     dataout = xr.Dataset()
     dataout[varname] = (dimnames, output)
     dataout[dimnames[0]] = ([dimnames[0]], ytt)
@@ -59,22 +72,31 @@ varnames = ['uo']
 #pref = 'speed_V'
 #varnames = ['vo']
     
+# Reads the entire file that corresponds to the prefix (grid_T, ptrc_T, etc.)
 dataglob = xr.open_mfdataset('%s/*%s*nc' %(dirin, pref), combine='by_coords')
 year = dataglob['time_counter.year'].values
 month = dataglob['time_counter.month'].values
 
+# sets the output directory
 dirout = '/home/datawork-marbec-pmod/outputs/APECOSM/ORCA1/corr_mask/output/yearly/pisces'
 
+# loop over all variables
 for v in varnames:
 
+    # if file exists, nothing is done.
     fileout = '%s/yearly_mean_%s.nc' %(dirout, v)
     if(os.path.isfile(fileout)):
         continue
 
     print('++++++++++++++++++++++++++++++++++ Computing %s yearly mean' %v)
-
+    
+    # extracts the data as xr.DataArray
     data = dataglob[v]
+    
+    # computes yearly mean and return a xr.Dataset
     output = compute_yearly_mean(year, month, data, v)
+    
+    # add attributes + save in netcdf
     output.attrs['file'] = os.path.realpath(__file__)
     output.attrs['date'] = str(datetime.today())
     output.to_netcdf(fileout)
