25b18abe9f77315609ad54b7b3abeb631dae3dbd
barriern <nicolas.barrier@ird.fr>
2020-12-16 16:17:53 +0100

    adding some files to misc

--- scripts/misc/misc.py
+++ scripts/misc/misc.py
@@ -1,3 +1,5 @@
+impor numpy as np
+
 def find_percentile(data, percentage=1):
 
     ''' 
@@ -19,3 +21,27 @@ def find_percentile(data, percentage=1):
     cmax = np.percentile(np.ravel(temp), 100 - percentage)
 
     return cmin, cmax
+
+def get_monthly_clim(var):
+    
+    ntime = var.shape[0]
+
+    nyears = ntime // 12
+
+    index = np.arange(12)
+
+    for i in range(nyears):
+        if(i == 0):
+            clim = var[index]
+        else:
+            clim += var[index]
+        index += 12
+    clim /= nyears
+
+    anom = np.zeros(var.shape)
+    index = np.arange(12)
+    for i in range(nyears):
+        anom[index] = var[index] - clim
+        index += 12
+
+    return clim, anom
