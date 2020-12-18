10a08c9f2b93869e074433ece06c1350d6925627
barrier.n <nicolas.barrier@ird.fr>
2020-12-18 06:47:18 +0000

    Update script to compute seasonal anomalies

--- scripts/chl-sat/compute_monthly_anom_chl.py
+++ scripts/chl-sat/compute_monthly_anom_chl.py
@@ -1,22 +1,25 @@
+import os.path
 from glob import glob
 import numpy as np
 import xarray as xr
 
-dataclim = xr.open_dataset("ESACCI-OC-L3S-CHLOR_A-MERGED-1M_MONTHLY_4km_GEO_PML_OCx-CLIM-fv5.0.nc")
+dirin = '/home1/datawork/nbarrier/chl-data/'
+
+dataclim = xr.open_dataset("%s/ESACCI-OC-L3S-CHLOR_A-MERGED-1M_MONTHLY_4km_GEO_PML_OCx-CLIM-fv5.0.nc" %dirin)
 clim = dataclim['clim_chlor_a'].to_masked_array()
 
-filelist = np.sort(glob('ESACCI-OC-L3S-CHLOR_A-MERGED-1M_MONTHLY_4km_GEO_PML_OCx-[0-9]*-fv5.0.nc'))
+filelist = np.sort(glob('%s/ESACCI-OC-L3S-CHLOR_A-MERGED-1M_MONTHLY_4km_GEO_PML_OCx-[0-9]*-fv5.0.nc' %dirin))
 
 for f in filelist:
 
     data = xr.open_dataset(f)
     month = data['time.month'].values[0] - 1 
-    print(f, month)
 
     chl = data['chlor_a']
 
     chl = chl - clim[month:month + 1]
 
-    fout = 'anom_%s' %f
+    fout = os.path.basename(f)
+    fout = '%s/anom_%s' %(dirin, fout)
 
     chl.to_netcdf(fout)
