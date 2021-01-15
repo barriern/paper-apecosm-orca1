73f12bdaf1ef4811db82345881a7bf987acb9d97
barriern <nicolas.barrier@ird.fr>
2021-01-15 18:35:11 +0100

    Update scrip[t

--- scripts/apecosm/cov/plot_final_cov_mean_epis.py
+++ scripts/apecosm/cov/plot_final_cov_mean_epis.py
@@ -10,6 +10,32 @@ from cartopy.mpl.geoaxes import GeoAxes
 import matplotlib.ticker as mticker
 #plt.rcParams['text.usetex'] = False
 
+def plot_domain(ax):
+    return
+    _plot_domain(ax, lonmin0, latmin0)
+    _plot_domain(ax, lonmin1, latmin1)
+    _plot_domain(ax, lonmin2, latmin2)
+    _plot_domain(ax, lonmin3, latmin3)
+    _plot_domain(ax, lonmin4, latmin4)
+    _plot_domain(ax, lonmin5, latmin5)
+
+def _plot_domain(axxx, lonmin, latmin):
+
+    lonout = [lonmin]
+    latout = [latmin]
+
+    proj = ccrs.PlateCarree(central_longitude=0)
+    proj2 = ccrs.PlateCarree(central_longitude=180)
+    axxx.plot(lonout, latout, transform=proj, zorder=1000, marker='o')
+
+lonmin0, latmin0 = 175, 5
+lonmin1, latmin1 = -150, -4
+lonmin2, latmin2 = 165, 8
+lonmin3, latmin3 = -175, -4
+
+lonmin4, latmin4 = -178, 3
+lonmin5, latmin5 = 145, 5
+
 # Define the settings of the grid 
 def set_grid(cpt):
 
@@ -18,7 +44,7 @@ def set_grid(cpt):
     gl.ylabels_right = False
     gl.xformatter = LONGITUDE_FORMATTER
     gl.yformatter = LATITUDE_FORMATTER
-    gl.xlocator = mticker.FixedLocator([150, 180, -180, -150, -120, -90, -60])
+    gl.xlocator = mticker.FixedLocator([150, 180, -150, -120, -90, -60])
 
 # converts to recover the value of the ax to used
 def get_cpt(cpt):
@@ -47,6 +73,7 @@ proj2 = ccrs.PlateCarree(central_longitude=0)
 
 ########################################################### reading mesh mask
 dirin = '/home/barrier/Work/apecosm/ORCA1/final_figs/data'
+dirin = '/home/barrier/Work/scientific_communication/articles/ongoing/paper-apecosm-orca1/scripts/data'
 
 mesh = xr.open_dataset('%s/mesh_mask_eORCA1_v2.2.nc' %dirin)
 mesh = mesh.isel(t=0, z=0)
@@ -100,14 +127,17 @@ cov = np.log10(cov)
 length = length[ilength]
 
 covout = np.ma.masked_where(cov.mask, covout)
-#cov = covout
-#cov = np.log10(cov)
+
+latmask = (np.abs(lat) <= 40)
+lonmask = (lon >= 130) & (lon <= 300)
+dommask = latmask & lonmask
+dommask = dommask[1:, 1:]
 
 for p in range(3):
 
     temp = cov[1:, 1:, p]
 
-    iok = np.nonzero(temp.mask == False)
+    iok = np.nonzero((temp.mask == False) & (dommask == True))
     cmax = np.percentile(temp[iok], 99)
     cmin = np.percentile(temp[iok], 1)
 
@@ -117,12 +147,13 @@ for p in range(3):
     ax.add_feature(cfeature.COASTLINE, zorder=101)
     cb = cbar_axes[get_cpt(cpt)].colorbar(cs)
     cb.set_label('Log10 Biom. dens. ($J.m^{-2}$)')
-    ax.set_ylim(-40, 40)
-    ax.set_xlim(-60, 130)
     cs.set_clim(cmin, cmax)
     ax.set_title('Length = %.2e cm' %length[p])
     ax.text(lontext, lattext, letters[cpt - 1] + ")", ha='center', va='center', transform=proj, bbox=dicttext)
     set_grid(cpt)
+    plot_domain(ax)
+    ax.set_ylim(-40, 40)
+    ax.set_xlim(-60, 130)
 
     cpt += 1
 
@@ -140,10 +171,9 @@ for p in range(3):
     temp = cov[p].T
     temp = temp[1:, 1:]
 
-    iok = np.nonzero(temp.mask == False)
+    iok = np.nonzero((temp.mask == False) & (dommask == True))
     perc = np.percentile(np.abs(temp[iok]), 99)
 
-    #ax = plt.subplot(sp0, sp1, get_cpt(cpt), projection=proj)
     ax = axout[get_cpt(cpt)]
     cs = ax.pcolormesh(lonf, latf, temp, transform=proj2, cmap=plt.get_cmap('RdBu_r'))
 
@@ -158,6 +188,7 @@ for p in range(3):
     set_grid(cpt)
     
     ax.text(lontext, lattext, letters[cpt - 1] + ")", ha='center', va='center', transform=proj, bbox=dicttext)
+    plot_domain(ax)
 
     cpt += 1
 
