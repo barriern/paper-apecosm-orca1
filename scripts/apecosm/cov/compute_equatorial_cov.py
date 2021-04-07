88d00013eada9468eb03aa7b9f992efa05a26e87
barrier.n <nicolas.barrier@ird.fr>
2021-04-07 08:25:49 +0000

    Update paths

--- scripts/apecosm/cov/compute_equatorial_cov.py
+++ scripts/apecosm/cov/compute_equatorial_cov.py
@@ -8,20 +8,22 @@ import scipy.signal as sig
 import re
 from glob import glob
 
-pattern = 'data/final-runs_(.*)_meridional_mean_anoms.nc'
+dirin = '/home1/datawork/nbarrier/apecosm/apecosm_orca1/diags/data'
+
+pattern = '%s/final-runs_(.*)_meridional_mean_anoms.nc' %dirin
 regexp = re.compile(pattern)
-filelist = glob('data/*meridional_mean_anoms*nc')
+filelist = glob('%s/*meridional_mean_anoms*nc' %dirin)
 filelist.sort()
 varlist = []
 for f in filelist:
     varname = regexp.match(f).groups()[0]
     varlist.append(varname)
 
-sys.path.append('/home/barrier/Work/scientific_communication/articles/ongoing/paper-apecosm-orca1/scripts/nino')
+sys.path.append('/home1/datahome/nbarrier/apecosm/configs/apecosm_orca1/diags/paper-apecosm-orca1/scripts/nino')
 from extract_nino import read_index
 
 dmin, dmax = 195801, 201812
-daten, nino = read_index(filename='/home/barrier/Work/apecosm/ORCA1/DATA_APE_ORCA1/apecosm/index/oni.data')
+daten, nino = read_index(filename='/home1/datahome/nbarrier/apecosm/configs/apecosm_orca1/diags/nino/index/oni.data')
 iok = np.nonzero((daten >= 195801) & (daten <= 201812))
 daten, nino = daten[iok], nino[iok]
 nnino = len(nino)
@@ -30,7 +32,7 @@ for varname in varlist:
     
     print('Processing variable ', varname)
 
-    data = xr.open_dataset('data/final-runs_%s_meridional_mean_anoms.nc' %varname)
+    data = xr.open_dataset('%s/final-runs_%s_meridional_mean_anoms.nc' %(dirin, varname))
     data = data.isel(community=0)
     lon = data['x'].values
     oope = data[varname].to_masked_array()
@@ -57,4 +59,4 @@ for varname in varlist:
     dsout['covariance'] = (['w', 'lon', 'lags'], covariance)
     dsout['lags'] = (['lags'], lags)
     dsout['lon'] = (['lon'], lon)
-    dsout.to_netcdf('data/equatorial_covariance_%s.nc' %varname)
+    dsout.to_netcdf('%s/equatorial_covariance_%s.nc' %(dirin, varname))
