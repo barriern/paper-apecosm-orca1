d46725ca60d3cb07806f9e34168462f93ee97524
barrier.n <nicolas.barrier@ird.fr>
2021-04-27 14:09:06 +0000

    Correct way to process

--- scripts/apecosm/cov/compute_covariance_monthly_tpi_epi_allsizes.py
+++ scripts/apecosm/cov/compute_covariance_monthly_tpi_epi_allsizes.py
@@ -11,10 +11,12 @@ dirout = '/home1/datawork/nbarrier/apecosm/apecosm_orca1/diags/data'
 
 prefix = 'final-runs'
 
+# read the data of filtered TPI
 index = xr.open_dataset('../../data/filt_tpi.nc')
 ensof = index['tpi_filt'].to_masked_array()
 ensoy = index['time'].to_masked_array()
 
+# extracts the time series of non-masked data.
 print(ensof.shape, ensoy.shape)
 iok = np.nonzero(ensof.mask == False)[0]
 
@@ -28,59 +30,58 @@ for varname in ['OOPE']:
 
     print("@@@@@@@@@@@@@@@@@@ ", varname)
 
-    data = xr.open_mfdataset('%s/*nc' %(dirin))
+    data = xr.open_mfdataset('%s/temp*nc' %(dirin))
     year = data['time.year'].values * 100 + data['time.month'].values 
 
     ymin = np.max([ensoy.min(), year.min()])
     ymax = np.min([ensoy.max(), year.max()])
 
+    # extract the time-series ovelap
     iok_enso = np.nonzero((ensoy <= ymax) & (ensoy >= ymin))[0]
     iok = np.nonzero((year <= ymax) & (year >= ymin))[0]
 
     print(ymin, ymax)
 
+    # extraction of good time-series
     data = data.isel(time=iok)
     enso = ensof[iok_enso]
     N = len(enso)
     
-    dimnames = data[varname].dims  # time, y, x, w
+    dimnames = list(data.dims)  # time, y, x, w
+    print(dimnames)
     dimnames = dimnames[::-1]  # w, x, y, time
 
     data = data[varname].to_masked_array()  # time, y, x, w
-    tmask = data.mask
     clim, data = ts.get_monthly_clim(data)
     del(clim)
-    data = np.ma.masked_where(tmask, data)
-    del(tmask)
-    data = np.ma.transpose(data)  # w, x, y, time
     print(data.shape)
-    print(enso.shape)
+    data = np.transpose(data)
+    print(data.shape)
 
     # extract the future size of cov. output
     shape1 = data.shape[:-1]  # w, x, y
     ntime = data.shape[-1]  
     shapetot = np.prod(shape1)
 
-    # resize data into (ntime, nall) arrays
-    data = np.ma.reshape(data, (shapetot, ntime))
-    isea = np.nonzero(data.mask[:, 0] == False)  
-
-    # detrending of time-series
-    data[isea, :] = sig.detrend(data[isea, :])
+    nw, nx, ny = shape1
 
     # extract the covariance into 1D form
-    cov = np.zeros(data[:, 0].shape)
+    cov = np.zeros(shape1)  # w, y, x
+    print(cov.shape)
+    print(data.shape)
 
     # data =  time, com, size, lat, lon
-    for i in isea[0]:
-        cov[i] = np.cov(data[i, :], enso, ddof=1)[0, 1]
-
-    # reshape covariance into it's proper form
-    cov = np.reshape(cov, (shape1))
+    for w in range(nw):
+        for i in range(nx):
+            for j in range(ny):
+                if np.any(np.isnan(data[w, i, j :])):
+                    continue
+                cov[w, i, j] = np.cov(sig.detrend(data[w, i, j :]), enso, ddof=1)[0, 1]
 
     fileout = '%s/%s_covariance_monthly_tpi_epis_%s.nc' %(dirin, prefix, varname)
     output = xr.Dataset()
-    output['covariance'] = (dimnames[:-1], cov)
+    output['covariance'] = (dimnames, cov)  # w, x, y
     output.attrs['file'] = os.path.realpath(__file__)
     output.attrs['date'] = str(datetime.datetime.today())
     output.to_netcdf(fileout)
+
