c4d4a299b98a2ba3c60e942284851cb937787c37
barriern <nicolas.barrier@ird.fr>
2021-04-29 17:09:07 +0200

    Update covariance

--- scripts/chl-sat/model/compute_covariance_monthly_mod.py
+++ scripts/chl-sat/model/compute_covariance_monthly_mod.py
@@ -1,18 +1,18 @@
 import xarray as xr
 import numpy as np
 import sys
-sys.path.append('../nino')
+sys.path.append('../../nino')
 from extract_nino import read_index
 import matplotlib.pyplot as plt
 import os
 
-dnino, nino = read_index('../data/index/oni.data')
+dnino, nino = read_index('../../data/index/oni.data')
 
 iok = np.nonzero((dnino >= 195801) & (dnino <= 201812))
 dnino = dnino[iok]
 nino = nino[iok]
 
-tpi = xr.open_dataset('../data/filt_tpi.nc')
+tpi = xr.open_dataset('../../data/filt_tpi.nc')
 dtpi = tpi['time'].values
 tpi = tpi['tpi_filt'].values
 
@@ -20,18 +20,18 @@ itpi = np.nonzero(np.isnan(tpi) == False)
 tpi = tpi[itpi]
 #tpi = (tpi - tpi[:].mean()) / np.std(tpi[:])
 
-data = xr.open_dataset('modsst_anoms.nc')
+data = xr.open_dataset('modchl_anoms.nc')
 #don = data['lon']
 #lat = data['lat']
-sst = data['sst'].to_masked_array()
+chl = data['chl'].to_masked_array()
 
-nx, ny, nt = sst.shape
+nx, ny, nt = chl.shape
 covtpi = np.zeros((nx, ny))
 covoni = np.zeros((nx, ny))
 
 for i in range(nx):
     for j in range(ny):
-        temp = sst[i, j]
+        temp = chl[i, j]
         covtpi[i, j] = np.cov(temp[itpi], tpi)[0, 1]
         covoni[i, j] = np.cov(temp, nino)[0, 1]
 
@@ -41,5 +41,5 @@ dsout = xr.Dataset()
 dsout['covtpi'] = (['x', 'y'], covtpi)
 dsout['covoni'] = (['x', 'y'], covoni)
 dsout.attrs['file'] = os.path.realpath(__file__)
-dsout.to_netcdf('cov_modsst_oni_tpi.nc')
+dsout.to_netcdf('cov_modchl_oni_tpi.nc')
 
