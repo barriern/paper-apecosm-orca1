e90a3772617f5954ad278d908dc0da5bc1dc35b7
barriern <nicolas.barrier@ird.fr>
2021-04-30 09:54:46 +0200

    Attempt for a detrending

--- scripts/chl-sat/interp/compute_chl_sat_covar_monthly.py
+++ scripts/chl-sat/interp/compute_chl_sat_covar_monthly.py
@@ -2,12 +2,18 @@ import xarray as xr
 import scipy.signal as sig
 import numpy as np
 import os.path
+import scipy.stats as stats
 
 import sys
 sys.path.append("../../nino")
 from extract_nino import read_index
 
-daten, nino = read_index(filename='/home/barrier/Work/apecosm/ORCA1/DATA_APE_ORCA1/apecosm/index/oni.data', keepnan=False)
+daten, nino = read_index(filename='../../data/index/oni.data', keepnan=False)
+
+data = xr.open_mfdataset("data/anom*nc", combine='by_coords', decode_times=False)
+realtime = data['time'].values
+print(realtime)
+print(realtime)
 
 data = xr.open_mfdataset("data/anom*nc", combine='by_coords')
 lat = data['lat'].values
@@ -17,6 +23,7 @@ date = date.values
 
 dmax = np.min([date.max(), daten.max()])
 dmin = np.max([date.min(), daten.min()])
+
 dmax = 201812
 
 inino = np.nonzero((daten <= dmax) & (daten >= dmin))
@@ -28,29 +35,32 @@ date = date[idata]
 data = data.isel(time=idata)
 data = data['chl_anom'].values
 
-print(date)
-print(daten)
-
 data = data.T  # time, lat, lon -> lon, lat, time
 
 nlon, nlat, ntime = data.shape
 
 output = np.zeros((nlon, nlat))
+count = np.zeros((nlon, nlat))
 
 for i in range(nlon):
     for j in range(nlat):
         temp = data[i, j]
-        iok = np.nonzero(temp == temp)[0]
+        iok = np.nonzero(np.isnan(temp) == False)[0]
+        count[i, j] = len(iok) / len(temp) * 100
         if len(iok) > 0:
+            slope, intercept, r_value, p_value, std_err = stats.linregress(realtime[iok], temp[iok])
+            trend = slope * realtime + intercept
+            temp -= slope
             output[i, j] = np.cov(temp[iok], nino[iok])[0, 1]
 
 output = output.T  # lat, lon
+count = count.T
 
 dsout = xr.Dataset({'lon': lon, 'lat':lat})
 dsout['cov'] = (['lat', 'lon'], output)
+dsout['count'] = (['lat', 'lon'], count)
 dsout.attrs['file'] = os.path.relpath(__file__)
 dsout.to_netcdf("covariance_satellite_data.nc")
-
 
 
 
