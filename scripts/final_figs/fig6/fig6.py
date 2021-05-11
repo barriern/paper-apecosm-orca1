aa44ebc6773f92e572cebcdcd1149a2e6a8aff1e
barriern <nicolas.barrier@ird.fr>
2021-05-11 09:27:59 +0200

    Update figure 6 (plot for all latitudes)

--- scripts/final_figs/fig6/fig6.py
+++ scripts/final_figs/fig6/fig6.py
@@ -16,12 +16,18 @@ from cycler import cycler
 from matplotlib.axes import Axes
 GeoAxes._pcolormesh_patched = Axes.pcolormesh
 
+wpred = [14, 45, 80]
+ilon = slice(58, 229, None)
+ilat = slice(114, 265, None)
+
 data = xr.open_dataset('../../data/ORCA1_JRAC02_CORMSK_CYC3_FINAL_ConstantFields.nc')
-data = data.isel(wpred=[14, 45, 80])
+data = data.isel(wpred=wpred)
 wstep = data['weight_step'].values
+length = data['length'].values  * 100
+print(length)
 print(wstep)
 
-dnino, nino = read_index(filename='data/index/oni.data')
+dnino, nino = read_index(filename='../../data/index/oni.data')
 ynino = dnino // 100
 iok = np.nonzero((ynino <= 2018) & (ynino >= 1958))
 nino = nino[iok]
@@ -32,69 +38,70 @@ proj2 = ccrs.PlateCarree()
 
 #mesh = xr.open_dataset("/Users/Nicolas/Work/sent/apecosm/ORCA1/mesh_mask_eORCA1_v2.2.nc")
 mesh = xr.open_dataset("../../data/mesh_mask_eORCA1_v2.2.nc")
+mesh = mesh.isel(x=ilon, y=ilat)
 lonf = mesh['glamf'].values[0]
 latf = mesh['gphif'].values[0]
 
-data = xr.open_dataset("data/eof_oni_annual_density_20.nc")
-eof = data['eofmap'].to_masked_array()
-pc = data['eofpc'].values
-var = data['eofvar'].values * 100
-time = data['time'].values
-ntime = len(time)
+for lll in range(20, 60, 10):
 
-years = np.array([t.year for t in time])
-month = np.array([t.month for t in time])
-time = np.arange(ntime)
-date = ['%.4d-%.2d' %(y, m) for y,m in zip(years, month)]
+    data = xr.open_dataset("data/eof_full_density_%d.nc" %lll)
+    data = data.isel(bins=wpred)
+    eof = data['eofmap'].to_masked_array()   # bins, eofs, y, x
+    pc = data['eofpc'].values
+    var = data['eofvar'].values * 100
+    time = data['time'].values
+    ntime = len(time)
 
-ncom, nbins, neof = var.shape
-coms = ['Epi.']
-sizes = ['3cm', '20cm', '90cm']
+    years = np.array([t.year for t in time])
+    month = np.array([t.month for t in time])
+    time = np.arange(ntime)
+    date = ['%.4d-%.2d' %(y, m) for y,m in zip(years, month)]
 
-dictgrid = {'crs':ccrs.PlateCarree(central_longitude=0), 'draw_labels':True, 'linewidth':0.5, 'color':'gray', 'alpha':0.5, 'linestyle':'--'}
-dicttext = dict(boxstyle='round', facecolor='lightgray', alpha=1)
-lontext = 120
-lattext = 30
-            
-fig = plt.figure(figsize=(12, 8))
-axes_class = (GeoAxes, dict(map_projection=proj))
-axgr = AxesGrid(fig, 111,  axes_class=axes_class, nrows_ncols=(3, 2), axes_pad=(1.23, 0.4), label_mode='', cbar_mode='each', cbar_pad=0.05)
-cbar_axes = axgr.cbar_axes
-axout = list(enumerate(axgr))
-axout = [p[1] for p in axout]
+    nbins, neof = var.shape
+    coms = ['Epi.']
+    sizes = ['3cm', '20cm', '90cm']
 
-cpt = 1
+    dictgrid = {'crs':ccrs.PlateCarree(central_longitude=0), 'draw_labels':True, 'linewidth':0.5, 'color':'gray', 'alpha':0.5, 'linestyle':'--'}
+    dicttext = dict(boxstyle='round', facecolor='lightgray', alpha=1)
+    lontext = 120
+    lattext = 30
+                
+    fig = plt.figure(figsize=(12, 8))
+    axes_class = (GeoAxes, dict(map_projection=proj))
+    axgr = AxesGrid(fig, 111,  axes_class=axes_class, nrows_ncols=(3, 2), axes_pad=(1.23, 0.4), label_mode='', cbar_mode='each', cbar_pad=0.05)
+    cbar_axes = axgr.cbar_axes
+    axout = list(enumerate(axgr))
+    axout = [p[1] for p in axout]
 
-letters = list(string.ascii_lowercase)
+    cpt = 1
 
-clim = [50, 5, 2]
+    letters = list(string.ascii_lowercase)
 
-for c in range(1):
+    clim = [50, 5, 2]
+
     for s in range(nbins):
         for e in range(2):
 
-            print("Comm = %d, Size = %d, EOF = %d" %(c, s, e + 1))
-
-            corrcoef = np.corrcoef(nino, pc[c, s, e, :])[0, 1]
+            corrcoef = np.corrcoef(nino, pc[s, e, :])[0, 1]
             if(corrcoef < 0):
-                eof[c, s, e, :, :] *= -1
-                pc[c, s, e, : ] *= -1
+                eof[s, e, :, :] *= -1
+                pc[s, e, : ] *= -1
 
-            eof[c, s, e]  *= wstep[s]
+            eof[s, e]  *= wstep[s]
 
             #ax = plt.subplot(3, 2, cpt, projection=proj)
             ax = axout[cpt - 1]
-            temp = np.abs(eof[c, s, e, 1:, 1:])
+            temp = np.abs(eof[s, e, 1:, 1:])
             perc = np.percentile(temp[temp.mask == False], 98)
             #clim = perc
             
-            cs = ax.pcolormesh(lonf, latf, eof[c, s, e, 1:, 1:], transform=proj2, cmap=plt.cm.RdBu_r)
+            cs = ax.pcolormesh(lonf, latf, eof[s, e, 1:, 1:], transform=proj2, cmap=plt.cm.RdBu_r)
             ax.add_feature(cfeature.LAND, color='lightgray')
             ax.add_feature(cfeature.COASTLINE)
             ax.set_ylim(-40, 40)
             ax.set_xlim(-60, 130)
             cs.set_clim(-clim[s], clim[s])
-            title = r'%s, %s, EOF %d (%.2f' %(coms[c], sizes[s], e + 1, var[c, s, e]) + '\%)'
+            title = r'%s, %s, EOF %d (%.2f' %('Epi.', sizes[s], e + 1, var[s, e]) + '\%)'
             print(title)
             ax.set_title(title)
             ax.text(lontext, lattext, letters[cpt - 1] + ")", ha='center', va='center', transform=proj, bbox=dicttext)
@@ -113,41 +120,5 @@ for c in range(1):
 
             cpt += 1
     
-            '''
-            ax = plt.subplot(3, 2, cpt, xmargin=-0.2)
-            print(cpt)
-            plt.plot(time, pc[c, s, e, :], color='black')
-            plt.fill_between(time, 0, nino, where=nino>0, interpolate=True, color='firebrick')
-            plt.fill_between(time, 0, nino, where=nino<0, interpolate=True, color='steelblue')
-            ax.set_xlim(time.min(), time.max())
-            stride =  5 * 12
-            ax.set_xticks(time[::stride])
-            ax.set_xticklabels(date[::stride], rotation=45, ha='right')
-            ax.set_ylim(-3, 3)
-            print(dir(ax))
-
-            cpt += 1
-
-            '''
-
-plt.savefig('eof_full.png', bbox_inches='tight')
-plt.close(fig)
-
-'''
-
-            ntime = len(nino) 
-            lags = sig.correlation_lags(ntime, ntime)
-            ilags = np.nonzero(np.abs(lags) <= 5 * 12)[0]
-            corr = sig.correlate(pc[c, s, e, :], nino) / ntime
-
-            lags = lags[ilags]
-            corr = corr[ilags]
-
-            imax = np.nonzero(np.abs(corr) == np.abs(corr).max())[0]
-            print(lags[imax], corr[imax])
-            
-            fig = plt.figure()
-            plt.plot(lags, corr)
-            plt.savefig('correlation_com_%d_size_%s_eof_%d' %(c, s, e), bbox_inches='tight')
-            plt.close(fig)
-'''
+    plt.savefig('fig6_%d.png' %lll, bbox_inches='tight')
+    plt.close(fig)
