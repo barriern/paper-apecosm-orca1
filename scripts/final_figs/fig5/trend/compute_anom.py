7ffdb5500cc05e1f0818d6eff18c040cda70853f
barrier.n <nicolas.barrier@ird.fr>
2021-06-07 14:21:17 +0000

    Update clim + add anom

--- scripts/final_figs/fig5/trend/compute_anom.py
+++ scripts/final_figs/fig5/trend/compute_anom.py
@@ -5,6 +5,10 @@ import os.path
 
 # open mesh file to extract x,y dimensions
 mesh = xr.open_dataset('../../../data/mesh_mask_eORCA1_v2.2.nc')
+lat = mesh['gphit'].values[0]
+ilat, ilon = np.nonzero(np.abs(lat) <= 40)
+ilat = slice(ilat.min(), ilat.max() + 1)
+mesh = mesh.isel(y=ilat)
 ny = mesh.dims['y']
 nx = mesh.dims['x']
 
@@ -12,36 +16,34 @@ dirin = '/home/datawork-marbec-pmod/outputs/APECOSM/ORCA1/final-runs/output'
 dirout = '/home1/datawork/nbarrier/apecosm/apecosm_orca1/diags/data/integrated'
 
 # list of variables to process
-varnames = ['madv_trend', 'zadv_trend', 'zdiff_trend', 'mdiff_trend']
+varnames = ['madv_trend', 'zadv_trend', 'zdiff_trend']
+varnames = ['mdiff_trend']
 
 for v in varnames:
     print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ", v)
+    
+    clim = xr.open_dataset('%s/clim_%s.nc' %(dirout, v))
+    clim = clim.isel(y=ilat)
+    clim = clim[v].to_masked_array()
 
     # extract the list of files
     pattern = '%s/*%s*nc' %(dirin, v)
     filelist = np.sort(glob(pattern))
     
-    # extract values of previous last step (init. 0)
-    output = np.zeros((12, ny, nx, 100))
-
     # loop over all the files
     for f in filelist:
         print(f)
         data = xr.open_dataset(f)
         time = data['time']
         # extract first community
-        data = data.isel(community=0)   # time, lat, lon, comm, w
-        data = data[v].to_masked_array()
+        data = data.isel(community=0, y=ilat)   # time, lat, lon, w
+        data = data[v].to_masked_array() - clim
+        
+        fout =  os.path.basename(f)
+        fout = '%s/anoms_%s' %(dirout, fout)
 
-        output += data
-
-    output /= len(filelist)
-       
-    # Save the data in a given directory
-    fout =  os.path.basename(f)
-    fout = '%s/clim_%s.nc' %(dirout, v)
-
-    dsout = xr.Dataset()
-    dsout[v] = (['time', 'y', 'x', 'w'], output)
-    dsout.attrs['file'] = os.path.realpath(__file__)
-    dsout.to_netcdf(fout, unlimited_dims='time')
+        dsout = xr.Dataset()
+        dsout['time'] = time
+        dsout[v] = (['time', 'y', 'x', 'w'], data)
+        dsout.attrs['file'] = os.path.realpath(__file__)
+        dsout.to_netcdf(fout, unlimited_dims='time')
