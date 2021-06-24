9434bc9f4a5a3f84f2c1bde9e6b84513fa3d1ef8
barriern <nicolas.barrier@ird.fr>
2021-06-24 10:11:17 +0200

    Adding jupytext comments

--- scripts/final_figs/fig1/cov-sst/extract_model_sst_anom_1958_2018.py
+++ scripts/final_figs/fig1/cov-sst/extract_model_sst_anom_1958_2018.py
@@ -1,8 +1,14 @@
+# # Step 2: Recovering detrended simulated SST anomalies
+#
+# ## Extracting the anomalies
+
 import xarray as xr
 import numpy as np
 import apecosm.ts as ts
 import scipy.signal as sig
 import os.path
+
+# **Note: here, the NEMO data has been extracted using `ncks -d olevel,0 ...`**
 
 data = xr.open_mfdataset("data/nemo/*nc")
 sst = data['thetao']
@@ -11,7 +17,11 @@ sst = np.squeeze(sst.to_masked_array())
 print(sst.shape)
 clim, sst = ts.get_monthly_clim(sst)  # time, lat, lon
 del(clim) 
-sst = sst.T  # x,.
+sst = sst.T  # lon, lat, time
+
+# ## Detrending the time-series
+#
+# Here, SST time-series has been transposed in order to improve computation time.
 
 nx, ny, nt = sst.shape
 
@@ -22,10 +32,9 @@ for i in range(nx):
         except:
             pass
 
+# ## Writting the output dataset
+
 dsout = xr.Dataset()
 dsout['sst'] = (('x', 'y', 'time'), sst)
-#dsout['time'] = time
-#dsout['lat'] = (['lat'], lat)
-#dsout['lon'] = (['lon'], lon)
 dsout.attrs['file'] = os.path.realpath(__file__)
 dsout.to_netcdf('modsst_anoms.nc')
