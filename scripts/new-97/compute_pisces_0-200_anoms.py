08db0bd2a7fc2c4185a1ed10c96871d1165895fd
barriern <nicolas.barrier@ird.fr>
2021-09-01 16:37:02 +0200

    Adding scripts to compute anomalies

---
+++ scripts/new-97/compute_pisces_0-200_anoms.py
@@ -0,0 +1,52 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import xarray as xr
+import matplotlib.pyplot as plt
+from dask.diagnostics import ProgressBar
+
+plt.rcParams['text.usetex'] = False
+
+zmax = 50
+varname = 'ZOO2'
+
+datestart = '1997-01'
+dateend = '1999-12'
+# -
+
+# ## Extracting the climatology
+#
+
+clim = xr.open_dataset('data/pacific_0-%d_%s_clim.nc' %(zmax, varname))
+clim
+
+varclim = clim[varname]
+varclim
+
+# ## Extracting the data
+
+data = xr.open_dataset('data/pacific_0-%d_%s.nc' %(zmax, varname)).sel(time_counter=slice(datestart, dateend))
+data
+
+var = data[varname]
+var
+
+# ## Computing the anomalies
+
+anom = var.groupby('time_counter.month') - varclim
+anom
+
+anom.to_netcdf('data/pacific_0-%d_%s_anom.nc' %(zmax, varname))
