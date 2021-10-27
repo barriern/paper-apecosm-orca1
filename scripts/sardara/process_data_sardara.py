988222f928c667025723b97f70e2dabdd692ee0d
barriern <nicolas.barrier@ird.fr>
2021-10-27 09:49:51 +0200

    Update script for Sardara analysis

--- scripts/sardara/process_data_sardara.py
+++ scripts/sardara/process_data_sardara.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.5
+#       jupytext_version: 1.10.3
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -47,7 +47,7 @@ data
 
 # ## Removing wrong geographic identifier
 
-# Now we remove areas which are not identified from integers
+# Now we remove areas which are not identified from integers. I.e when the identifier has missing values
 
 area = data.iloc[:, 4].values.astype(str)
 area
@@ -55,10 +55,21 @@ area
 pattern = '[0-9]+'
 regex = re.compile(pattern)
 
-test = np.array([(regex.match(a) is not None) for a in area])
+test = np.array([regex.match(a) is not None for a in area])
 
 datanew = data.iloc[test, :]
 datanew
 
 datanew = datanew.assign(geographic_identifier=datanew.loc[:, 'geographic_identifier'].astype(int))
 datanew
+
+# Now we extract the data whose codes are in the Pacific domain:
+
+test_index = np.array([v in areacodes for v in datanew['geographic_identifier'].values])
+iok = np.nonzero(test_index == True)[0]
+len(iok)
+
+dataout = datanew.iloc[iok, :]
+dataout
+
+dataout.to_csv('data/processed_sardara_data_latmax_%d_lonmin_%d_lonmax_%d.csv' %(pacific_latmax, pacific_lonmin, pacific_lonmax))
