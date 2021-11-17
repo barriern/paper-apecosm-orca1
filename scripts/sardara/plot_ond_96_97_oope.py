2c95dc3747de71ac0456c9e06c43a52927c6ff93
barriern <nicolas.barrier@ird.fr>
2021-11-17 16:36:40 +0100

    Update OOPE script

--- scripts/sardara/plot_ond_96_97_oope.py
+++ scripts/sardara/plot_ond_96_97_oope.py
@@ -29,6 +29,9 @@ const = const.rename({'wpred': 'w'})
 const = const.assign(w=const['length'].values * 100)
 const
 
+yearend = 2009
+yearinit = yearend - 1
+
 ilat = slice(None, -3)
 # -
 
@@ -36,9 +39,9 @@ mesh = xr.open_dataset('../new-97/data/pacific_mesh_mask.nc').isel(z=0, y=ilat)
 lonf = mesh['glamf'].values
 latf = mesh['gphif'].values
 
-data = xr.open_dataset('../new-97/data/pacific_nino97_OOPE.nc').isel(y=ilat)
+data = xr.open_mfdataset('data/pacific_*OOPE*.nc').isel(y=ilat)
 data = data.assign(w=const['length'] * 100)
-data
+data['time'].values
 
 tot = data['OOPE'] * const['weight_step']
 tot = tot.sel(w=slice(25, 75))
@@ -55,15 +58,16 @@ def extract_ond(year):
     dateend
 
     catch_96 = tot.sel(time=slice(datestart, dateend))
+    print(catch_96)
     output = catch_96.mean(dim='time')
     return output
 
 
-catch_96 = extract_ond(1996)
-catch_96
+catch_init = extract_ond(yearinit)
+catch_init
 
-catch_97 = extract_ond(1997)
-catch_97
+catch_end = extract_ond(yearend)
+catch_end
 
 # +
 plt.rcParams['font.size'] = 15
@@ -77,9 +81,9 @@ axes_class = (GeoAxes, dict(map_projection=projout))
 axgr = ImageGrid(fig, 111,  axes_class=axes_class, nrows_ncols=(3, 1), axes_pad=(0.0, 0.6), label_mode='',
                 cbar_mode='each', cbar_size=0.1, cbar_pad=0.3, cbar_location="right", share_all=True)
 
-diff = catch_97 - catch_96
-tp_catch_97 = catch_97.where(catch_97 != 0)
-tp_catch_96 = catch_96.where(catch_96 != 0)
+diff = catch_end - catch_init
+tp_catch_end = catch_end.where(catch_end != 0)
+tp_catch_init = catch_init.where(catch_init != 0)
 diff = diff.where(diff != 0)
 
 gridparams = {'crs':ccrs.PlateCarree(central_longitude=0), 'draw_labels':True, 'linewidth':0.5, 
@@ -92,14 +96,14 @@ ccc = 500
 #cmax = 3
 
 ax = axgr[0]
-cs = ax.pcolormesh(lonf, latf, tp_catch_96.values[1:, 1:], transform=projin, cmap=plt.cm.jet)
+cs = ax.pcolormesh(lonf, latf, tp_catch_init.values[1:, 1:], transform=projin, cmap=plt.cm.jet)
 cb = plt.colorbar(cs, axgr.cbar_axes[0])
 cb.set_label('Catch (MT)')
 ax.add_feature(cfeature.LAND, zorder=2)
 ax.add_feature(cfeature.COASTLINE, zorder=3)
 ax.set_extent([120.5, 299.5, -39.5, 39.5], crs=projin)
 cs.set_clim(cmin, cmax)
-ax.set_title('OND 96')
+ax.set_title('OND %d' %yearinit)
 gl = ax.gridlines(**gridparams)
 gl.top_labels = False
 gl.right_labels = False
@@ -107,15 +111,14 @@ gl.xformatter = LONGITUDE_FORMATTER
 gl.yformatter = LATITUDE_FORMATTER
 gl.xlocator = mticker.FixedLocator([150, 180, -180, -150, -120, -90, -60])
 
-
 ax = axgr[1]
-cs = ax.pcolormesh(lonf, latf, tp_catch_97.values[1:, 1:], transform=projin, cmap=plt.cm.jet)
+cs = ax.pcolormesh(lonf, latf, tp_catch_end.values[1:, 1:], transform=projin, cmap=plt.cm.jet)
 cb = plt.colorbar(cs, axgr.cbar_axes[1])
 cb.set_label('Catch (MT)')
 ax.add_feature(cfeature.LAND)
 ax.add_feature(cfeature.COASTLINE)
 cs.set_clim(cmin, cmax)
-ax.set_title('OND 97')
+ax.set_title('OND %d' %yearend)
 gl = ax.gridlines(**gridparams)
 gl.top_labels = False
 gl.right_labels = False
@@ -130,10 +133,10 @@ cb.set_label('Catch (MT)')
 ax.add_feature(cfeature.LAND)
 ax.add_feature(cfeature.COASTLINE)
 cs.set_clim(-ccc, ccc)
-ax.set_title('OND 97 - OND 96')
+ax.set_title('OND %d - OND %d' %(yearend, yearinit))
 ax.add_feature(cfeature.LAND)
 ax.add_feature(cfeature.COASTLINE)
-plt.savefig('plot_ond_sardara_catches.png', bbox_inches='tight')
+
 gl = ax.gridlines(**gridparams)
 gl.top_labels = False
 gl.right_labels = False
@@ -141,7 +144,7 @@ gl.xformatter = LONGITUDE_FORMATTER
 gl.yformatter = LATITUDE_FORMATTER
 gl.xlocator = mticker.FixedLocator([150, 180, -180, -150, -120, -90, -60])
 
-plt.savefig('plot_ond_oope_diff.png', bbox_inches='tight')
+plt.savefig('plot_ond_oope_diff_%d_%d .png' %(yearinit, yearend), bbox_inches='tight')
 # -
 
 
