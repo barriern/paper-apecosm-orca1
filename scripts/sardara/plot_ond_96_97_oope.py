b1d30de998d9fb3c0695faf8ae565c5f87486a73
barriern <nicolas.barrier@ird.fr>
2021-11-17 09:07:21 +0100

    adding script for apecosm + adding grid lines

--- scripts/sardara/plot_ond_96_97_oope.py
+++ scripts/sardara/plot_ond_96_97_oope.py
@@ -24,30 +24,38 @@ import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import matplotlib.ticker as mticker
 
-skj = xr.open_dataset('data/regridded_catch_gear_PS_species_SKJ_1x1.nc')
-skj
+const = xr.open_dataset('../data/ORCA1_JRAC02_CORMSK_CYC3_FINAL_ConstantFields.nc').isel(gpred=0)
+const = const.rename({'wpred': 'w'})
+const = const.assign(w=const['length'].values * 100)
+const
+
+ilat = slice(None, -3)
 # -
 
-yft = xr.open_dataset('data/regridded_catch_gear_PS_species_YFT_1x1.nc')
-yft
+mesh = xr.open_dataset('../new-97/data/pacific_mesh_mask.nc').isel(z=0, y=ilat)
+lonf = mesh['glamf'].values
+latf = mesh['gphif'].values
 
-tot = yft['catch'] + skj['catch']
+data = xr.open_dataset('../new-97/data/pacific_nino97_OOPE.nc').isel(y=ilat)
+data = data.assign(w=const['length'] * 100)
+data
 
-dates = tot['time'].values
+tot = data['OOPE'] * const['weight_step']
+tot = tot.sel(w=slice(25, 75))
+tot = tot.sum(dim='w')
+tot
 
 
 def extract_ond(year):
     
-    datestart = year * 100 + 10
+    datestart = '%.4d-10-01' %year
     datestart
 
-    dateend = year * 100 + 12
+    dateend = '%.4d-12-31' %year
     dateend
 
-    iok = np.nonzero((dates >= datestart) & (dates <= dateend))[0]
-    
-    catch_96 = tot.isel(time=iok)
-    output = catch_96.sum(dim='time')
+    catch_96 = tot.sel(time=slice(datestart, dateend))
+    output = catch_96.mean(dim='time')
     return output
 
 
@@ -69,30 +77,27 @@ axes_class = (GeoAxes, dict(map_projection=projout))
 axgr = ImageGrid(fig, 111,  axes_class=axes_class, nrows_ncols=(3, 1), axes_pad=(0.0, 0.6), label_mode='',
                 cbar_mode='each', cbar_size=0.1, cbar_pad=0.3, cbar_location="right", share_all=True)
 
-lon = catch_96['lon'].values
-lat = catch_96['lat'].values
-
 diff = catch_97 - catch_96
 tp_catch_97 = catch_97.where(catch_97 != 0)
 tp_catch_96 = catch_96.where(catch_96 != 0)
 diff = diff.where(diff != 0)
 
-cmax = 1000
-cmin = 100
-ccc = 1000
-#cmin = 0
-#cmax = 3
-
 gridparams = {'crs':ccrs.PlateCarree(central_longitude=0), 'draw_labels':True, 'linewidth':0.5, 
               'color':'k', 'linestyle':'--'}
 
+cmax = 500
+cmin = 100
+ccc = 500
+#cmin = 0
+#cmax = 3
+
 ax = axgr[0]
-cs = ax.pcolormesh(lon, lat, tp_catch_96.values, transform=projin, shading='auto', cmap=plt.cm.jet)
+cs = ax.pcolormesh(lonf, latf, tp_catch_96.values[1:, 1:], transform=projin, cmap=plt.cm.jet)
 cb = plt.colorbar(cs, axgr.cbar_axes[0])
 cb.set_label('Catch (MT)')
 ax.add_feature(cfeature.LAND, zorder=2)
 ax.add_feature(cfeature.COASTLINE, zorder=3)
-ax.set_extent([lon.min(), lon.max(), lat.min(), lat.max()], crs=projin)
+ax.set_extent([120.5, 299.5, -39.5, 39.5], crs=projin)
 cs.set_clim(cmin, cmax)
 ax.set_title('OND 96')
 gl = ax.gridlines(**gridparams)
@@ -102,8 +107,9 @@ gl.xformatter = LONGITUDE_FORMATTER
 gl.yformatter = LATITUDE_FORMATTER
 gl.xlocator = mticker.FixedLocator([150, 180, -180, -150, -120, -90, -60])
 
+
 ax = axgr[1]
-cs = ax.pcolormesh(lon, lat, tp_catch_97.values, transform=projin, shading='auto', cmap=plt.cm.jet)
+cs = ax.pcolormesh(lonf, latf, tp_catch_97.values[1:, 1:], transform=projin, cmap=plt.cm.jet)
 cb = plt.colorbar(cs, axgr.cbar_axes[1])
 cb.set_label('Catch (MT)')
 ax.add_feature(cfeature.LAND)
@@ -118,7 +124,7 @@ gl.yformatter = LATITUDE_FORMATTER
 gl.xlocator = mticker.FixedLocator([150, 180, -180, -150, -120, -90, -60])
 
 ax = axgr[2]
-cs = ax.pcolormesh(lon, lat, diff.values, transform=projin, shading='auto')
+cs = ax.pcolormesh(lonf, latf, diff.values[1:, 1:], transform=projin, shading='auto')
 cb = plt.colorbar(cs, axgr.cbar_axes[2])
 cb.set_label('Catch (MT)')
 ax.add_feature(cfeature.LAND)
@@ -127,6 +133,7 @@ cs.set_clim(-ccc, ccc)
 ax.set_title('OND 97 - OND 96')
 ax.add_feature(cfeature.LAND)
 ax.add_feature(cfeature.COASTLINE)
+plt.savefig('plot_ond_sardara_catches.png', bbox_inches='tight')
 gl = ax.gridlines(**gridparams)
 gl.top_labels = False
 gl.right_labels = False
@@ -134,9 +141,7 @@ gl.xformatter = LONGITUDE_FORMATTER
 gl.yformatter = LATITUDE_FORMATTER
 gl.xlocator = mticker.FixedLocator([150, 180, -180, -150, -120, -90, -60])
 
-plt.savefig('plot_ond_sardara_catches.png', bbox_inches='tight')
-
-lon.min(), lon.max(), lat.min(), lat.max()
+plt.savefig('plot_ond_oope_diff.png', bbox_inches='tight')
 # -
 
 
