2bcc8520f728dfdd93b900216dfcf29480200961
barriern <nicolas.barrier@ird.fr>
2021-12-10 16:59:23 +0100

    Update time-series

---
+++ scripts/new-97/plot_oope_time_series.py
@@ -0,0 +1,62 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import xarray as xr
+import matplotlib.pyplot as plt
+plt.rcParams['text.usetex'] = False
+
+const = xr.open_dataset('data/ORCA1_JRAC02_CORMSK_CYC3_FINAL_ConstantFields.nc')
+length = const['length'].values * 100
+const = const.rename({'wpred' : 'l'})
+const['l'] = length
+const
+
+mesh = xr.open_dataset('data/pacific_mesh_mask.nc').isel(z=0)
+mesh
+
+mask = mesh['tmaskutil']
+mask.plot()
+
+lon = mesh['glamt']
+lat = mesh['gphit']
+
+weights = mesh['e1t'] * mesh['e2t'] * mesh['tmaskutil']
+weights.plot()
+
+west = (abs(lat) < 5) & (lon >= 0)
+weights.where(west).plot()
+
+east = (abs(lat) < 5) & (lon <= 0)
+weights.where(east).plot()
+
+data = xr.open_dataset('data/pacific_nino97_OOPE.nc')
+oope = data['OOPE']
+oope
+
+clim = xr.open_dataset('data/pacific_clim_OOPE.nc')
+oopeclim = clim['OOPE']
+oopeclim
+
+oope = oope.rename({'w': 'l'})
+oope['l'] = length
+oope = oope * const['weight_step']
+
+oope_west = (oope * weights.where(west)).sum(dim=['x', 'y'])
+oope_west.name = 'OOPE_west'
+oope_west.sel(l=3, method='nearest').plot()
+
+oope_east = (oope * weights.where(east)).sum(dim=['x', 'y'])
+oope_east.name = 'OOPE_east'
+oope_east.sel(l=3, method='nearest').plot()
