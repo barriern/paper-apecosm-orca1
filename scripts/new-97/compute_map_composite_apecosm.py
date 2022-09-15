a358ca4a4c42bba7df7b068929da3f6437f08f93
barrier.n <nicolas.barrier@ird.fr>
2022-09-15 11:27:55 +0000

    Update script

--- scripts/new-97/compute_map_composite_apecosm.py
+++ scripts/new-97/compute_map_composite_apecosm.py
@@ -18,38 +18,38 @@ import xarray as xr
 from glob import glob
 import numpy as np
 
-varname = 'growthTrend'
+for varname in ['madv_trend', 'zadv_trend', 'zdiff_trend', 'mdiff_trend']:
+
+    filelist = []
+    for y in [1982, 1997, 2015]:
+        pattern = '/home1/scratch/nbarrier/pacific_ORCA1*%s*%d*.nc' %(varname, y)
+        temp = glob(pattern)[0]
+        filelist.append(temp)
+    print(filelist)
+
+    data = xr.open_mfdataset(filelist)[varname]
+    print(data)
+
+    clim = xr.open_dataset('data/pacific_clim_%s.nc' %varname)[varname]
+    print(clim)
+
+    anom = data.groupby('time.month') - clim
+    print(anom)
+
+    months = anom['time.month'].values
+    iok = np.nonzero(months >=10)[0]
+    print(iok)
+
+    anom = anom.isel(time=iok)
+    print(anom['time'])
+
+    compo = anom.mean(dim='time')
+    print(compo)
+
+    fileout = 'data/composite_%s_map.nc' %varname
+    print(fileout)
+
+    compo.to_netcdf(fileout)
 # -
 
-filelist = []
-for y in [1982, 1997, 2015]:
-    pattern = '/home1/scratch/nbarrier/*%s_Y%.4d*nc' %(varname, y)
-    print(pattern)
-    temp = glob(pattern)[0]
-    filelist.append(temp)
-filelist
 
-data = xr.open_mfdataset(filelist)[varname]
-data
-
-clim = xr.open_dataset('data/pacific_clim_%s.nc' %varname)[varname]
-clim
-
-anom = data.groupby('time.month') - clim
-anom
-
-months = anom['time.month'].values
-iok = np.nonzero(months >=10)[0]
-iok
-
-anom = anom.isel(time=iok)
-anom['time']
-
-compo = anom.mean(dim='time')
-compo
-
-fileout = 'data/composite_%s_map.nc' %varname
-fileout
-
-
-compo.to_netcdf(fileout)
--- scripts/new-97/test-trend-timeseries.py
+++ scripts/new-97/test-trend-timeseries.py
@@ -8,25 +8,30 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.13.7
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python [conda env:nbarrier2]
 #     language: python
-#     name: python3
+#     name: conda-env-nbarrier2-py
 # ---
 
+# +
 import xarray as xr
 import matplotlib.pyplot as plt
 import numpy as np
 
-oope0 = xr.open_dataset('data/test/test_pacific_ORCA1_JRAC02_CORMSK_CYC3_FINAL_OOPE_init_Y1958D000.nc')
+ytt = [1982, 1997, 2015]
+mtt = [10, 11, 12]
+# -
+
+oope0 = xr.open_dataset('/home1/scratch/nbarrier/test_pacific_ORCA1_JRAC02_CORMSK_CYC3_FINAL_OOPE_init_Y1958D000.nc')
 oope0 = oope0['OOPE_init'].squeeze()
 oope0 = float(oope0)
 
 
 def process_time_series(varname):
-    oope = xr.open_mfdataset('data/test/*%s*.nc' %varname)[varname].squeeze()
+    oope = xr.open_mfdataset('/home1/scratch/nbarrier/test_pacific_*%s*.nc' %varname)[varname].squeeze()
     oope
-    datestart = '1971-01-01'
-    dateend = '2000-12-31'
+
+    dates = oope['time.year'] * 100 + oope['time.month']
 
     oope_clim = oope.groupby('time.month').mean(dim='time')
     oope_clim
@@ -43,23 +48,23 @@ def process_time_series(varname):
     output['clim'] = oope_clim
     output['anom'] = oope_anoms
     
-    return output
+    return output, dates
 
 
 # +
 varnames = [
-    'growthTrend',
     'madv_trend',
-    'mdiff_trend',
-    'predationTrend',
     'zadv_trend',
-    'zdiff_trend'
+    'mdiff_trend',
+    'zdiff_trend',
+    'predationTrend',
+    'growthTrend',
 ]
 
 output = {}
 
 for v in ['OOPE'] + varnames:
-    temp = process_time_series(v)
+    temp, dates = process_time_series(v)
     output[v] = temp
 output.keys()
 # -
@@ -68,8 +73,9 @@ plt.figure()
 oope_clim = output['OOPE']['clim']
 oope_anoms = output['OOPE']['anom']
 oope_tot = oope_anoms + oope_clim
-plt.plot(oope_tot)
-plt.plot(oope_clim + oope_anoms)
+plt.plot(oope_tot - oope_tot.mean())
+plt.plot(oope_anoms)
+plt.plot(oope_clim - oope_clim.mean())
 
 test = 0
 for v in varnames[:]:
@@ -108,7 +114,51 @@ print(np.corrcoef(trend, oope_tot[1:]))
 
 plt.figure(figsize=(12, 8))
 plt.plot(output['OOPE']['anom'])
+
 plt.plot(trend)
+# -
+list_dates = [y * 100 + m for y in ytt for m in mtt]
+list_dates
+
+test_dates = [d in list_dates for d in dates[1:].values]
+idates = np.nonzero(test_dates)[0]
+dates[1:][idates]
+
+# +
+series = {}
+for v in varnames:
+    
+    temp = output[v]['anom']
+    temp = np.cumsum(temp)
+    temp = 0.5 * (temp[1:] + temp[:-1])
+    series[v] = temp
+
+plt.figure(figsize=(12, 8))
+cpt = 1
+for v in varnames:
+    plt.subplot(3, 2, cpt)
+    plt.plot(series[v], marker='.')
+    plt.plot(idates, series[v][idates], marker='o', linestyle='none')
+    plt.title(v)
+    cpt += 1
+# -
+
+compo_oope_anom = output['OOPE']['anom'][1:][idates].mean()
+compo_oope_anom
+
+# +
+toto = 0
+for t in series:
+    print(t)
+    toto += series[t]
+    
+plt.figure(figsize=(12, 8))
+plt.plot(output['OOPE']['anom'][1:])
+plt.plot(toto)
+plt.plot(idates, output['OOPE']['anom'][1:][idates], marker='.', linestyle='none')
+plt.plot(idates, toto[idates], marker='.', linestyle='none')
+
+toto[idates].mean(), output['OOPE']['anom'][1:][idates].mean()
 # -
 
 
