6f1aab2adf0d05e04fbe4f6104c568835a73f258
barriern <nicolas.barrier@ird.fr>
2023-03-10 18:21:47 +0100

    Adding script

--- scripts/zenodo-scripts/fig3/apecosm/integrate_biomass_30-60cm-10N-10S_full.py
+++ scripts/zenodo-scripts/fig3/apecosm/integrate_biomass_30-60cm-10N-10S_full.py
@@ -6,29 +6,29 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.3
+#       jupytext_version: 1.10.3
 #   kernelspec:
-#     display_name: Python [conda env:nbarrier] *
+#     display_name: Python 3 (ipykernel)
 #     language: python
-#     name: conda-env-nbarrier-py
+#     name: python3
 # ---
 
 import xarray as xr
 from dask.diagnostics import ProgressBar
 
-mesh = xr.open_dataset('/home/datawork-marbec-pmod/forcings/APECOSM/ORCA1_HINDCAST/corrected_mesh_mask_eORCA1_v2.2.nc')
-mesh = mesh.isel(t=0, z=0)
+mesh = xr.open_dataset('../../data/static/pacific_mesh_mask.nc')
+mesh = mesh.isel(z=0)
 lat = mesh['gphit']
 mesh = mesh.where(abs(lat) <= 10)
 surf = mesh['e1t'] * mesh['e2t'] * mesh['tmaskutil']
 surf.plot()
 
-const = xr.open_dataset('/home/datawork-marbec-pmod/outputs/APECOSM/ORCA1/final-runs/output/ORCA1_JRAC02_CORMSK_CYC3_FINAL_ConstantFields.nc')
+const = xr.open_dataset('../../data/static/ORCA1_JRAC02_CORMSK_CYC3_FINAL_ConstantFields.nc')
 const = const.rename({'wpred': 'l'})
 const['l'] = const['length'] * 100
 const
 
-data = xr.open_mfdataset('/home/datawork-marbec-pmod/outputs/APECOSM/ORCA1/final-runs/output/*OOPE*Y*nc')
+data = xr.open_mfdataset('../../data/apecosm/*OOPE*Y*nc')
 data = data.isel(community=0)
 data = data.rename({'w': 'l'})
 data['l'] = const['l']
