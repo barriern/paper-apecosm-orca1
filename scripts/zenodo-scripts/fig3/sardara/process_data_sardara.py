3bed2e61c9d852743d4011d835dee55f86b50951
barriern <nicolas.barrier@ird.fr>
2023-03-10 17:51:35 +0100

    Adding processing sardara datasets

---
+++ scripts/zenodo-scripts/fig3/sardara/process_data_sardara.py
@@ -0,0 +1,83 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Processing Sardara data
+#
+# Data provided by Jonathan Rault (Datarmor: `/home/datawork-marbec-pmod/outputs/APECOSM/ORCA1_REF/analyses/sardara/data-analysis/sardara/julien-dataset/data`)
+#
+# The aim of this script is to:
+# - Remove the data with bad geographic identifiers (countries instead of codes)
+# - Remove the data which are not in the Pacific Ocean, based on the comparison with geographic identifiers
+#
+# ## Imports
+
+# +
+import pandas as pd
+import numpy as np
+import re
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+
+pacific_latmax = 40
+pacific_lonmin = 120
+pacific_lonmax = -60
+# -
+
+# ## Reading processed areas
+
+areas = pd.read_csv('processed_sardara_areas_latmax_%d_lonmin_%d_lonmax_%d.csv' %(pacific_latmax, pacific_lonmin, pacific_lonmax), index_col=0)
+areas
+
+areacodes = areas.loc[:, 'code'].values
+areacodes
+
+# ## Reading the CSV
+
+data = pd.read_csv('../../data/external/global_catch_ird_level2.csv', sep=',')
+data
+
+# ## Removing wrong geographic identifier
+
+# Now we remove areas which are not identified from integers, i.e when the identifier is a string and not a number.
+
+area = data.loc[:, 'geographic_identifier'].values.astype(str)
+area
+
+pattern = '[0-9]+'
+regex = re.compile(pattern)
+
+test = np.array([regex.match(a) is not None for a in area])
+
+datanew = data.iloc[test, :]
+datanew
+
+# Now we convert the identifier from string to integer.
+
+datanew = datanew.assign(geographic_identifier=datanew.loc[:, 'geographic_identifier'].astype(int))
+datanew
+
+# Now we extract the data whose codes are in the Pacific domain. To do so, we compare the input geographic identifier with the ones of the Pacific area files.
+
+test_index = np.array([v in areacodes for v in datanew['geographic_identifier'].values])
+iok = np.nonzero(test_index == True)[0]
+len(iok)
+
+# Finally, we extract the data for these locations and we save the new file.
+
+dataout = datanew.iloc[iok, :]
+dataout
+
+dataout.to_csv('processed_sardara_data_latmax_%d_lonmin_%d_lonmax_%d.csv' %(pacific_latmax, pacific_lonmin, pacific_lonmax))
---
+++ scripts/zenodo-scripts/fig3/sardara/process_data_sardara_L2.py
@@ -0,0 +1,79 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Second processing of the Sardara file.
+#
+# This script aims at providing a new processing to the Sardara file. Starting from the data file extracted for the Pacific, this script aims at:
+# - Extract monthly data and discard yearly and 3-monthly data
+# - Aggregate the catches that share the same area, starting date, gear and species.
+
+# +
+import pandas as pd
+import numpy as np
+import re
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+import re
+from mpl_toolkits.axes_grid1 import ImageGrid
+plt.rcParams['text.usetex'] = False
+
+pacific_latmax = 40
+pacific_lonmin = 120
+pacific_lonmax = -60
+# -
+
+# First, the pre-processed file is loaded:
+
+data = pd.read_csv('processed_sardara_data_latmax_%d_lonmin_%d_lonmax_%d.csv' %(pacific_latmax, pacific_lonmin, pacific_lonmax), index_col=0)
+data
+
+
+# Then, we convert the time-start column into a dates using regular expressions. First, we create a function that does that:
+
+def make_dates(data, arg):
+    dates = data.loc[:, 'time_' + arg]
+    regex = re.compile('([0-9][0-9][0-9][0-9])\-([0-9][0-9])\-([0-9][0-9])')
+    yymmdd = np.array([regex.match(d).groups() for d in dates]).astype(int)
+    yy = yymmdd[:, 0]
+    mm = yymmdd[:, 1]
+    dd = yymmdd[:, 2]
+    dictout = {'year_' + arg:yy, 'month_' + arg: mm, 'day_' + arg: dd, 'date_' + arg: 10000*yy + mm * 100 + dd}
+    data = data.assign(**dictout)
+    return data
+
+
+# The years and months are added to the file:
+
+data = make_dates(data, 'start')
+data = make_dates(data, 'end')
+data
+
+# Now we remove data which are not monthly ones:
+
+test = data.loc[:, 'month_end'] - data.loc[:, 'month_start']
+np.unique(test)
+
+data = data[test == 0]
+data
+
+# Now, we sum the catches that have the same dates, geographic area and species (this is to take into account that different countries may fish on the same areas at the same dates).
+
+dataint = data.groupby(['time_start', 'geographic_identifier', 'species', 'gear']).sum().loc[:, 'value']
+dataint
+
+dataint.to_csv('processed_sardara_data_L2_latmax_%d_lonmin_%d_lonmax_%d.csv' %(pacific_latmax, pacific_lonmin, pacific_lonmax))
+
+
